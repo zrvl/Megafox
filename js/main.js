61e02d8dec90921896b160cbea89431075c282fb
zrvl <zhuravelbogdan0@gmail.com>
2022-04-05 12:12:46 +0300

    added content and slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(function() {
-  
+  $('.top-slider__inner').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    speed: 1000,
+  })
+  $('.mid-slider__inner').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    speed: 1000,
+  })
 })
